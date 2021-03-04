5598683762dad3add259003ffd4d9a0d90dcecd9
Dhia Ayachi <dhia.ayachi@bell.ca>
2021-03-04 14:26:28 -0500

    Add unit tests

--- BingoApi.go
+++ BingoApi.go
@@ -10,50 +10,51 @@ const baseUrl = "https://api.bing.microsoft.com/v7.0/"
 
 // This struct formats the answer provided by the Bing News Search API.
 type NewsAnswer struct {
-	ReadLink       string `json: "readLink"`
-	QueryContext   struct {
-		OriginalQuery   string   `json: "originalQuery"`
-		AdultIntent     bool        `json: "adultIntent"`
+	ReadLink     string `json: "readLink"`
+	QueryContext struct {
+		OriginalQuery string `json: "originalQuery"`
+		AdultIntent   bool   `json: "adultIntent"`
 	} `json: "queryContext"`
-	TotalEstimatedMatches   int  `json: totalEstimatedMatches"`
-	Sort  []struct {
-		Name    string  `json: "name"`
-		ID       string    `json: "id"`
-		IsSelected       bool  `json: "isSelected"`
-		URL      string   `json: "url"`
-	}  `json: "sort"`
-	Value   []struct   {
-		Name     string   `json: "name"`
-		URL   string    `json: "url"`
-		Image   struct   {
-			Thumbnail   struct  {
-				ContentUrl  string  `json: "thumbnail"`
-				Width   int  `json: "width"`
-				Height  int   `json: "height"`
+	TotalEstimatedMatches int `json: totalEstimatedMatches"`
+	Sort                  []struct {
+		Name       string `json: "name"`
+		ID         string `json: "id"`
+		IsSelected bool   `json: "isSelected"`
+		URL        string `json: "url"`
+	} `json: "sort"`
+	Value []struct {
+		Name  string `json: "name"`
+		URL   string `json: "url"`
+		Image struct {
+			Thumbnail struct {
+				ContentUrl string `json: "thumbnail"`
+				Width      int    `json: "width"`
+				Height     int    `json: "height"`
 			} `json: "thumbnail"`
 		} `json: "image"`
-		Description  string  `json: "description"`
-		Provider  []struct   {
-			Type   string    `json: "_type"`
-			Name  string     `json: "name"`
+		Description string `json: "description"`
+		Provider    []struct {
+			Type string `json: "_type"`
+			Name string `json: "name"`
 		} `json: "provider"`
-		DatePublished   string   `json: "datePublished"`
+		DatePublished string `json: "datePublished"`
 	} `json: "value"`
 }
 
 type BingoApi struct {
-	ClientKey     string
+	ClientKey string
 }
 
 func New(ClientKey string) *BingoApi {
 	return &BingoApi{ClientKey}
 }
+
 type Args struct {
 	Key   string
 	Value string
 }
 
-func (b *BingoApi) newsSearch(q string) (*NewsAnswer, error) {
+func (b *BingoApi) NewsSearch(q string) (*NewsAnswer, error) {
 	url := baseUrl + "news/search"
 
 	req, err := http.NewRequest("GET", url, nil)
@@ -63,7 +64,7 @@ func (b *BingoApi) newsSearch(q string) (*NewsAnswer, error) {
 	param := req.URL.Query()
 	req.Header.Add("Ocp-Apim-Subscription-Key", b.ClientKey)
 	param.Add("q", q)
-	param.Add("freshness","Day")
+	param.Add("freshness", "Day")
 
 	res, err := http.DefaultClient.Do(req)
 
@@ -73,13 +74,13 @@ func (b *BingoApi) newsSearch(q string) (*NewsAnswer, error) {
 	// Read the results
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
 	ans := new(NewsAnswer)
 	err = json.Unmarshal(body, ans)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
-	return ans,nil
+	return ans, nil
 }
---
+++ BingoApi_test.go
@@ -0,0 +1,40 @@
+package BingoApi
+
+import (
+	"os"
+	"testing"
+)
+
+func TestBingoApi_NewsSearch(t *testing.T) {
+	ClientKey := os.Getenv("CLIENT_KEY")
+
+	type fields struct {
+		ClientKey string
+	}
+	type args struct {
+		q string
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{"test basic call", fields{ClientKey}, args{"microsoft"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := &BingoApi{
+				ClientKey: tt.fields.ClientKey,
+			}
+			got, err := b.NewsSearch(tt.args.q)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NewsSearch() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if len(got.Value) <= 0 {
+				t.Errorf("NewsSearch() got = %v, want %v", got, tt.args.q)
+			}
+		})
+	}
+}
